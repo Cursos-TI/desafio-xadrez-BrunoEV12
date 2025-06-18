39bccff69e0903b91e1da51b413a678e4d194fc7
BrunoEV12 <brunodev5405@gmail.com>
2025-06-18 23:03:49 +0000

    Versão Definitiva.
    +adiconado novas variaveis.
    
    Recursos de movimentação alterados.
    
    +Torre, Bispo e Rainha agora usam Recursividade para se mover.
    +Alem da Recursividade o Bispo também usa Loops Aninhados.
    +Adiconado Loops Complexos para movimentação do Cavalo.

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,84 @@
 #include <stdio.h>
 
+//recursividade torre
+void torre(int numero){
+
+if( numero <= 5){ //if para controla repetição da recursividade, na linha 10 o numero soma, quando passa o valor , o loop parar.
+
+    printf("Torre se mova a Direita \n"); //Imprimir para usuario
+    torre(numero + 1); //chama rescursividade somando mais 1 ao valor
+
+}
+
+}
+
+
+
+
+
+
+
+
+//recusividade Bispo
+void bispo(int numero){
+
+    int bispo1movimentação = 1, bispo2movimentação;
+
+
+if( numero <= 5){  //if para controla repetição da recursividade, na linha 46 o numero soma, quando passa o valor , o loop parar.
+
+    while (bispo1movimentação < 2) //repetição while
+    {
+        printf("Bispo se mova a Cima, ");  //Imprimir para usuario
+
+        for (bispo2movimentação = 1; bispo2movimentação < 2; bispo2movimentação++) //repetição for dentro do while
+        {
+            printf("Direita \n"); //Imprimir para usuario 
+        }
+        
+        bispo1movimentação++; //inclemento while pra parar o loop
+
+
+    }
+
+    
+    bispo(numero + 1); //chama rescursividade somando mais 1 ao valor
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+//recusividade Rainha
+void rainha(int numero){
+
+if( numero < 8){ //if para controla repetição da recursividade, na linha 66, o numero soma, quando passa o valor , o loop parar.
+
+    printf("Rainha se mova a Esquerda \n");//Imprimir para usuario 
+    rainha(numero + 1); //chama rescursividade somando mais 1 ao valor
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 int main(){
 
 
@@ -10,9 +89,8 @@ int main(){
 //Variaveis do Programa
 
 //Variaveis de peças
-int torre = 1, bispo = 1, rainha, movimento1Cavalo = 1, movimento2Cavalo; //varivael (Torre recebe valor 1), (Bispo recebe valor 1). 
-
-
+int quantidadeBispo = 1, quantidadeRainha, movimento1Cavalo = 1, movimento2Cavalo, movimento3Cavalo; //varivael (Torre recebe valor 1), (Bispo recebe valor 1). 
+int quantidadeTorre = 1;
 
 
 
@@ -31,16 +109,8 @@ printf("============================ \n");
 
 //Logica de movimentação
 
-while (torre <= 5) //estrutura de repetição.
-{
-    
-    printf("Torre se mova a Direita \n");
-    
-    torre++; //Inclemento, valor soma 1(EX: se i = 1 ,i++ sera 2 e assim em diante)
-      
-}
 
-
+torre(quantidadeTorre); //chama recursividade
 
 
 
@@ -65,15 +135,7 @@ printf("============================ \n");
 
 //Logica de movimentação
 
-do
-{
-    
-    printf("Bispo se mova a Cima,Direita \n");
-
-    bispo++; //Inclemento, valor soma 1(EX: se i = 1 ,i++ sera 2 e assim em diante)
-    
-  
-} while (bispo <= 5);
+bispo(quantidadeBispo); //chama recursividade
 
 
 
@@ -98,14 +160,8 @@ printf("============================ \n");
 
 //Logica de movimentação
 
-for (rainha = 1; rainha <= 8; rainha++)
-{
-    
-    
-    printf("Rainha se mova a Esquerda \n");
-    
-}
 
+rainha(quantidadeRainha);  //chama recursividade
 
 
 
@@ -129,15 +185,15 @@ printf("============================ \n");
 
 //Logica de movimentação
 
-while (movimento1Cavalo--)
+while (movimento1Cavalo--) //repetição while
 {
-    for (movimento2Cavalo = 0; movimento2Cavalo < 2; movimento2Cavalo++)
+    for (movimento2Cavalo = 0, movimento3Cavalo = 2; movimento2Cavalo < movimento3Cavalo; movimento2Cavalo++) //repetição for com multiplas variaveis, dentro do while.
     {
         
-        printf("Baixo \n");
+        printf("cima \n");  //Imprimir para usuario.
     }
     
-    printf("esquerda \n");
+    printf("direita \n");  //Imprimir para usuario.
 
 
 }
